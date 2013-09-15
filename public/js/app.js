be43fd6889c60dc8db6df8e5fdb1dd67aa803777
Shawn Lim Quan Cheng <shawnlim89@live.com>
2013-09-15 21:06:45 +0800

    some bugs

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,10 @@
 'use strict';
 
 // Declare app level module which depends on filters, and services
-window.app = angular.module('mean', ['ngCookies', 'ngResource', 'ui.router', 'ui.bootstrap']);
+window.app = angular.module('mean', ['ngCookies', 'ngResource', 'ui.router', 'ui.bootstrap', 'mean.site', 'mean.auth', 'mean.articles']);
 
 // bundling dependencies
-/*window.angular.module('mean.site', ['mean.site.home_ctrl']);
+window.angular.module('mean.site', ['mean.site.home_ctrl', 'mean.site.header_ctrl']);
 window.angular.module('mean.auth', []);
-window.angular.module('mean.articles', []);*/
+window.angular.module('mean.articles', []);
 
